60639ced7a7ebfc143c3991836b0331893e8be1d
RicardoRR29 <r3ifes@gmail.com>
2020-12-22 07:39:27 -0300

    keycodes fixed

--- event_keycodes/script.js
+++ event_keycodes/script.js
@@ -16,8 +16,5 @@ window.addEventListener('keydown', (e) => {
             <small>event.code</small>
         </div>
         
-        <div class="key">
-            Press any key to get the keyCode
-        </div>
     `
 })
