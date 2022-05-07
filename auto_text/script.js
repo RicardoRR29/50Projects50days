8112009a1b6981026cb74ae1b543fe4f8c0faada
Ricardo Ribeiro <r3ifes@gmail.com>
2022-05-06 22:04:21 -0300

    adicionando novos projetos

--- auto_text/script.js
+++ auto_text/script.js
@@ -1,21 +1,35 @@
 const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = "We Love Programming!"
+const titleList = [
+  'a Front End Especialist',
+  'a Christhian Developer',
+  'a Problem Solver',
+  'the Developer you need!',
+]
+let titleIdx = 0
 let idx = 1
-let speed = 300 / speedEl.value
+let speed = 300 / 10
+let valor = 1
 
 writeText()
 
 function writeText() {
-  textEl.innerText = text.slice(0, idx)
-  
-  idx++
-  
+  textEl.innerText = titleList[titleIdx].slice(0, idx)
+  let text = titleList[titleIdx]
+  idx = idx + valor
+  if(idx == 0) {
+    valor = +1
+    titleIdx++
+    if(titleIdx > text.length) {
+      titleIdx = 0
+    }
+  }
   if(idx > text.length) {
-    idx = 1
+    valor = -1
+    setTimeout(writeText, speed + 1000)
+  } else {
+    setTimeout(writeText, speed)
   }
   
-  setTimeout(writeText, speed)
 }
 
 speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
